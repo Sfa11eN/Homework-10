3f34917c3d164b2ce4871af730d5cd355e36ab7c
Sfa11eN <sfallenn@gmail.com>
2024-01-08 12:07:08 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" Тема Строки");
         task1();
-        }
-        public static void task1(){
-            System.out.println("Задание 1");
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = String.join(" ", lastName, firstName, middleName);
+
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("Задание 2");
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+
+    public static void task2() {
+        System.out.println("Задание 3");
+
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника-"+fullName.replace('ё','е'));
     }
 }
